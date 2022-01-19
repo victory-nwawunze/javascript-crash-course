11559c72b1c990e8f2e18b0e541875858ea9dfb6
victory-nwawunze <victorynwawunze@gmail.com>
2022-01-19 06:58:18 -0800

    numbers in javascript

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /** @format */
 
 console.log("hello worlds");
-alert("hello");
+// alert("hello");
 // This is a comments
 // alert("this is a comments")
 
@@ -15,5 +15,30 @@ var someNumber = 45;
 console.log(someNumber);*/
 
 // document.getElementById("someText").innerHTML = "Hey";
-var age = prompt("what your age?");
-document.getElementById("someText").innerHTML = age;
+// var age = prompt("what your age?");
+// document.getElementById("someText").innerHTML = age;
+
+// Function/Numbers in javascript
+// var num1 = 5.5;
+// console.log(5 * 3);
+// Increment
+// var num1 = 10;
+// num1 = num1 + 1;
+// console.log(num1);
+// var num1 = 20;
+// num1++;
+// console.log(num1);
+
+// Decrement numbers in javascript
+// var num1 = 30;
+// num1 = num1 - 10;
+// console.log(num1);
+// Or
+// var num1 = 10;
+// num1--;
+// console.log(num1);
+
+// Increment/Decrement by any number you want
+var num1 = 30;
+num1 += 10;
+console.log(num1);
